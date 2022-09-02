b31fe91982edeb976046847409508f8a4d68ec4b
Lucas Mariano <lucas.svmariano@gmail.com>
2022-09-02 09:23:21 -0300

    aula16 finished

---
+++ aula16/fatiamento_strings.py
@@ -0,0 +1,17 @@
+"""
+Manipulando strings
+* String indices --> string[index]
+* Fatiamento de strings [inicio:fim:passo] --> o fim não é incluido
+* Funções built-in(embutidas) len, abs, type, print, etc...
+Essas funções podem ser usadas diretamente em cada tipo
+
+"""
+# positivos     [012345678]
+texto         = 'Python s2'
+# negativos    -[987654321]
+
+# fatiamento
+nova_string = texto[:-3]
+string = texto[0::2]
+print(string)
+
